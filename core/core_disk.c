63585ced77d425cd8af46d74f7e1d94a0a281d64
bbbradsmith <bbbradsmith@users.noreply.github.com>
2023-08-22 16:52:17 -0400

    set_initial_image should return false to indicate intentional non-support (RetroArch still wants to notify about this for some reason, tho)

--- core/core_disk.c
+++ core/core_disk.c
@@ -661,11 +661,12 @@ static bool set_initial_image(unsigned index, const char* path)
 	retro_log(RETRO_LOG_DEBUG,"set_initial_image(%d,%p)\n",index,path);
 	if(path) retro_log(RETRO_LOG_DEBUG,"path: %s\n",path); // logging this path but not doing anything with it
 	//initial_image = index;
+	//return true;
 	// Disabling this. RetroArch appears to give you the last disk you had in the drive, last time you played.
 	// This is inappropriate for Atari ST, where you almost always need a boot disk to start the game.
 	// It also conflicts with the drive B workaround, where RetroArch won't know which drive it is saving the last index of.
 	initial_image = 0;
-	return true;
+	return false;
 }
 
 static bool get_image_path(unsigned index, char* path, size_t len)
